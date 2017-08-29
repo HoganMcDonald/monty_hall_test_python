ac64439cb4a5d2432209aab94ff7cf35950109b9
Hogan McDonald <hpeter.mcdonald@icloud.com>
2017-08-28 19:08:25 -0500

    Basic functions written. next: remove one of the false options as a viable choice.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # monty_hall_test_python
 A statistical simulation of the monty hall problem.
+
+I watched a video on the Monty Hall problem by vox and decided to create a python script to test possible answers. [This](https://www.youtube.com/watch?v=ggDQXlinbME) video explains it much better than I could in a readme. 
+
+This script will generate a random set of guesses given a population size, then will either generate a set of random switched guesses or just the initial set and return ultimately the success percentage. variables are easy to configure, which makes it easy to compare very large populations for increased statistical accuracy.
--- monty_hall.py
+++ monty_hall.py
@@ -3,15 +3,9 @@ import random as r
 
 
 # constants
-population_size = 100
+population_size = 10000
 change_answer = True
-
-
-# cluster of doors
-doors = {'0': True, '1': False, '2': False}
-
-i = r.randint(0, 2)
-print(str(i), doors[str(i)])
+doors = {'0': True, '1': False, '2': False}  # cluster of doors
 
 
 # create list of initial gueses (expect equal distribution)
@@ -37,6 +31,38 @@ def return_count(population):
     return {'0': a, '1': b, '2': c, 'total': a + b + c}
 
 
+# takes initial pop and bool for changing guess on second round. Creates a new population of final guesses.
+def second_guess(population, change_bool):
+    # array to be returned
+    population_2 = []
+    # loop through existing population
+    for i in population:
+        # if meant to change
+        if change_bool:
+            new_guess = i
+            # will continue generating new guesses until it doesn't match first guess. Simulates
+            # changing mind while preserving randomness of second guess.
+            while new_guess == i:
+                new_guess = r.randint(0, 2)
+            population_2.append(new_guess)
+        # will result in identical array returned
+        else:
+            return population
+    return population_2
+
+
+# returns the percentage of guesses that were correct
+def percent_correct(population):
+    total_correct = 0
+    for i in population:
+        if doors[str(i)]:
+            total_correct += 1
+    return total_correct / len(population)
+
+
 pop = generate_initial_distribution(population_size)
-print('initial population: ', pop)
-print('count of each guess: ', return_count(pop))
+# print('initial population: ', pop)
+print('count of each guess in initial distribution: ', return_count(pop))
+second_pop = second_guess(pop, change_answer)
+print('count of each guess in final distribution: ', return_count(second_pop))
+print('the accuracy of this method was: ', percent_correct(second_pop))
